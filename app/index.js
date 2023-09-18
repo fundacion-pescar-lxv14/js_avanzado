24c8fe3862ef7f0e7969ba42c65d4d822341c18b
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-09-18 15:57:11 -0300

    definir eventos y funciones navegacion principal

--- app/index.js
+++ app/index.js
@@ -18,4 +18,26 @@ const
 d.addEventListener('DOMContentLoaded', function () {
     console.log('Pagina Cargada Completamente');
 
+    // Captura de elementos del DOM
+    const 
+        cL = 'classList',
+        className = 'active',
+        nav_menu = get('#nav .nav-menu'),
+        nav_toggle = get('#nav .icon-toggle')
+
+    // Funciones del DOM
+    function toggle({el, btn, c, className}){
+        el[cL].toggle(className) ?
+            btn[cL].replace(c[0], c[1]) :
+            btn[cL].replace(c[1], c[0])
+    }
+
+    // Eventos del DOM
+    nav_toggle.addEventListener('click', (e) => toggle({
+        btn: e.target, // elemento al que apunta el evento
+        c: ['icon-toggle', 'icon-close'], // clases a reemplazar
+        el: nav_menu, // elemento a modificar
+        className, // corresponde a mismo nombre de clave y valor
+    } ) );
+    
 } )
