0a828b758a09f03b3d94df833954f9a7fdbbf9f3
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-02 15:28:19 -0300

    definir funciones de tabla, listas y error

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 const 
     jph = "https://jsonplaceholder.typicode.com",
     control = {
-        user: 0,
-        post: 0,
-        album: 0
+        user: null,
+        post: null,
+        album: null,
+        todo: null
     },
     events = ['click', 'input', 'check', 'change', 'select', 'submit']
 //#consultas asincronas 
@@ -19,22 +20,81 @@ async function getData({url, method = 'GET', callback }){
 //#consultas asincronas
 //#funciones de renderizado 
 const section = ({ title, content, callback} ) => `
-<h2 class="h3 text-center">${title}</h2>
-<div>${ callback(content) }</div>
-<a href="#">volver</a>`;
+    <h2 class="h3 text-center">${title}</h2>
+    <div>${ callback(content) }</div>`;
 
+const error = (e = 'No se ha Iniciado sesion') => section({ 
+    title: 'Lo sentimos, ha ocurrido un error', 
+    content: e, 
+    callback: (e) => `
+        <p class="alert alert-danger alert-dismissible">${e} 
+            <button class="btn-close float-end"></button>
+        </p>`
+});
+
+function renderList(item, li=''){
+    Object.keys(item).map(k => 
+        li+= typeof item[k] == 'object' ? 
+        renderList(item[k]) :
+        `<li><strong>${k}:</strong> ${item[k]}</li>`)
+    return `<ul>${li}</ul>`
+}
 function usersList(data, list = ''){
     data.map(item => list += `
-        <li class="list-group-item | col-12 col-md-8 col-lg-6 | d-flex flex-wrap | mx-auto">
+        <li class="list-group-item | col-12 col-md-10 col-lg-8 | d-flex flex-wrap | mx-auto">
             <p class="col d-grid m-0">
                 <strong>${item.username}</strong>
-                <span>${item.email}</span>
+                <em>${item.email}</em>
             </p>
             <button id="user-${item.id}" class="btn btn-primary rounded-pill">seleccionar</button>
         </li>`
     );
     return `<ul class="list-group">${list}</ul>`;
 }
+function userTodos(data, th = '', tr = '', td = '') {
+    // Extraccion de Cabeceras de Tabla
+    Object.keys(data[0]).map(t => th+=`<th>${t}</th>`)
+    // Identificacion de Filas y Celdas
+    data.map(item => ( 
+        Object.values(item).map(v => 
+        td+= (typeof v == 'object') ? 
+            `<td>${renderList(item)}</td>` : 
+            `<td>${v}</td>`
+        ),
+        tr += `<tr>${td}</tr>`,
+        td=''
+    ));
+    // Renderizado condicional de Tabla 
+    return control.user ?
+    `<div class="container table-responsive">
+        <table class="table-striped table-hover table-sm">
+            <caption>Table de datos</caption>
+            <thead><tr>${th}</tr></thead>
+            <tbody>${tr}</tbody>
+        </table>
+    </div>`
+    : error() 
+}
+function userAlbums(){
+    if(control.user){
+
+    }
+    else{
+        error()
+    }
+}
+function userPosts(){
+    if(control.user){
+
+    }
+    else{
+        error()
+    }
+}
+function postComments(){
+
+}
+
 //#funciones de renderizado
 //#eventos del DOM
 document.addEventListener('DOMContentLoaded', () => {
