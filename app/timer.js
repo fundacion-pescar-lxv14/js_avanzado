151b5a9eea4a3a7c3f2ea45a2362885c5dcdb3f3
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-04 21:13:07 -0300

    agregar parametros de plantilla reloj

--- app/timer.js
+++ app/timer.js
@@ -3,41 +3,45 @@ import Clock from './clock.js';
 import { startTime, Start, stopTime } from './interval.js'
 
 const clockTemplate = (name, time) => `
-    <section id="timer">
+    <section id="timer" class="d-grid">
         <h2>${name}</h2>
-        <select>
+        <select class="form-control">
             <option value="rem">Temporizador</option>
             <option value="add" selected>Cronometro</option>
         </select>
-        <p>HH:mm:ss</p>
+        <p>HH:MM:ss</p>
         <p class="clock">${time}</p>
         <div class="d-flex">
-            <button id="start">Iniciar</button>
-            <button id="stop">Detener</button>
+            <button id="start" class="btn">Iniciar</button>
+            <button id="stop" class="btn">Detener</button>
         </div>
     </section>`;
 
 const clockActions = () => {
+    let obj = new Clock(0,0,0);
     const 
         start = d.getElementById('start'),
         stop = d.getElementById('stop');
         
-        start.onclick = () => {
-            const fn = d.querySelector('#timer select').value;
-            // Evitamos que Continue el intervalo
-            stopTime();
-            // Iniciamos un Nuevo Intervalo
-            startTime( () => Start({ 
-                obj, fn, 
-                c: fn == 'add' ? 0 : 59, 
-                next: d.querySelector('.clock').innerHTML = timer.showTime() 
-            }))
-        }
-        stop.onclick = () => stopTime()
+    start.onclick = () => {
+        const fn = d.querySelector('#timer select').value;
+        // Evitamos que Continue el intervalo
+        stopTime();
+        // Iniciamos un Nuevo Intervalo
+        startTime( () => Start({ 
+            obj, fn, 
+            c: fn == 'add' ? 0 : 59, 
+            next: d.querySelector('.clock').innerHTML = obj.showTime() 
+        }))
     }
+    stop.onclick = () => {
+        stopTime()
+        obj = new Clock(0,0,0);
+        d.querySelector('.clock').innerHTML = obj.showTime()
+    }
+}
 function renderClock(){
     // Instancia de Clock
-    const obj = new Clock(0,0,0);
     root.innerHTML += clockTemplate('Reloj', '00:00:00');
     clockActions();
 }
