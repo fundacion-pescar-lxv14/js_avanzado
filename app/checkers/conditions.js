ede161fbc0928c85e3e6a2bdb10c381a085e5d65
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-09 16:20:01 -0300

    corregir formato numero en condicion mover

--- app/checkers/conditions.js
+++ app/checkers/conditions.js
@@ -5,4 +5,4 @@ export const coord = (r,c, n = 0) => r % 2 == n && c % 2 == n
 // La celda deber estar vacia
 export const empty = (element) => element.children.length === 0
 // La ficha se mueve en diagonal
-export const move = (from, to, p = 1) => from == (to + (11 * p)) || from == (to + (9 * p))
+export const move = (from, to, p = 1) => (from == (parseInt(to) + (11 * p)) || from == (parseInt(to) + (9 * p)))
