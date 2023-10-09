76c1aa24c313ce494fb82f5bc7fca0b5c1a88731
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-09 16:20:56 -0300

    definir botones de accion

--- app/checkers/box.js
+++ app/checkers/box.js
@@ -3,7 +3,10 @@ import checker from './checker.js';
 
 function box(){
     const colors = ['red', 'black'];
-    const box = Object.assign(d.createElement('div'), { className: 'box'})
+    const box = Object.assign(d.createElement('div'), { 
+        id: 'checkers-box',
+        className: 'box'
+    })
     colors.forEach( (color, i) => { 
         const player = i + 1;
         for(let index = 1; index <= 12; index++){
--- app/checkers/index.js
+++ app/checkers/index.js
@@ -1,11 +1,25 @@
-import { d, root } from '../global.js';
+import { d, local, root } from '../global.js';
 import box from './box.js';
 import board from './board.js';
+import order from './order.js';
+import { read, write} from '../storage.js';
 
 function Checkers(){
     const section = Object.assign(d.createElement('section'), {className: "d-grid"});
     section.append(
         board(),
+        Object.assign(d.createElement('button'), {
+            className: 'btn btn-primary',
+            innerHTML: 'Iniciar Juego',
+            onclick: () => order()
+        }),
+        Object.assign(d.createElement('button'),{
+            innerHTML: 'Guardar Partida',
+            onclick: () => {
+                write('board', d.getElementById('checkers-board').innerHTML, local);
+                write('box', d.getElementById('checkers-box').innerHTML, local)
+            }
+        }),
         box(),
     );
     root.append(section);
